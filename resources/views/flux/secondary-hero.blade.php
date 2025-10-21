bc46c2763152a893cc7ab30117e3457e4d3f5139
Dominic Naufal <dom@Dominics-MacBook-Air.local>
2025-10-21 17:00:01 +0800

    feat: UI on Shop Page

--- resources/views/flux/secondary-hero.blade.php
+++ resources/views/flux/secondary-hero.blade.php
@@ -1,10 +1,10 @@
-@props(['text'])
+@props(['text', 'description'])
 
-<div class="md:h-64 h-44 w-full bg-center flex  justify-center items-center bg-cover bg-no-repeat" style="background-image: url({{ asset('assets/2nd.jpg') }})">
+<div class="md:h-[80vh] h-full w-full bg-center flex  justify-center items-center bg-cover bg-no-repeat" style="background-image: url({{ asset('assets/shop/banner.png') }})">
     {{-- <flux:container class=""> --}}
-        <div class="md:mt-4 mt-12">
-            <div class="text-center md:text-2xl  text-white">{{ $text }}</div>
-            <div class="mt-4 md:text-lg text-center text-white ">Home > {{ $text }}</div>
+        <div class="md:mt-4 mt-12 w-[70%]">
+            <div class="text-center md:text-4xl text-black font-medium">{{ $text }}</div>
+            <div class="mt-4 md:text-xl text-center text-black ">{{ $description }}</div>
         </div>
     {{-- </flux:container> --}}
 </div>
--- resources/views/livewire/shop-index.blade.php
+++ resources/views/livewire/shop-index.blade.php
@@ -1,9 +1,11 @@
 <div>
-    <flux:secondary-hero text="Shop"></flux:secondary-hero>
+    <flux:secondary-hero text="Our Products"
+        description="Discover our delightful range of freshly baked breads and pastries, crafted to bring joy to your taste buds every morning.">
+    </flux:secondary-hero>
 
     <flux:container>
-        <div class="flex justify-center gap-4 mt-10">
-            <flux:button wire:click='toogleFilter'>Filter</flux:button>
+        <div class="flex justify-center gap-2 mt-10">
+            {{-- <button wire:click='toogleFilter' class="cursor-pointer px-4 rounded-xl hover:bg-[#CFAF8D] text-sm bg-[#D4A373]">Filter</button> --}}
             <flux:input icon="magnifying-glass" wire:model.live='search' class="w-1/2!"
                 :placeholder="'Search Our Product Here'"></flux:input>
             {{-- <flux:button icon="" variant="primary"></flux:button> --}}
@@ -11,51 +13,59 @@
     </flux:container>
 
 
-    <flux:container>
+    <flux:container class="mt-4">
         <div class="flex flex-wrap md:flex-nowrap gap-4">
-            @if ($filter)
-                <div class="w-full md:w-1/4">
-                    <div class="font-semibold">Filters</div>
+            {{-- @if ($filter) --}}
+            <div class="w-full sticky top-24 md:w-1/4 bg-white p-4 rounded-lg shadow-md h-fit">
+                <div class="font-semibold">Filters</div>
 
-                    <div class="mt-4 mb-2">Category</div>
-                    <flux:radio.group wire:model.live='category' class="">
-                        @foreach ($categories as $item)
-                            <flux:radio :label="$item->name" :value="$item->slug"></flux:radio>
-                        @endforeach
-                    </flux:radio.group>
+                <div class="mt-4 mb-2">Category</div>
+                <flux:radio.group wire:model.live='category' class="">
+                    @foreach ($categories as $item)
+                        <flux:radio :label="$item->name" :value="$item->slug"></flux:radio>
+                    @endforeach
+                </flux:radio.group>
 
 
-                    <div class="mt-4">Price Range</div>
-                    <div class="flex gap-4">
-                        <flux:input wire:model.live='min' max="{{ $max }}" type="number" step="500"
-                            min="0" :placeholder="'Min'"></flux:input>
-                        <div class="">-</div>
-                        <flux:input wire:model.live='max' min="{{ $min }}" type="number" step="500"
-                            min="0" :placeholder="'Max'"></flux:input>
-                    </div>
-
-                    <div class="mt-4 mb-2">Freshness</div>
-                    <flux:radio.group wire:model.live='freshness' class="">
-                        <flux:radio :label="'Fresh'" :value="'fresh '"></flux:radio>
-                        <flux:radio :label="'Frozen'" :value="'frozen'"></flux:radio>
-                    </flux:radio.group>
-
-                    <div class="flex gap-4 mt-4 justify-center">
-                        <flux:button variant="primary" wire:click='resetFilter' class="cursor-pointer" color="rose" >Reset</flux:button>
-                    </div>
+                <div class="mt-8">Price Range</div>
+                <div class="flex items-center gap-4">
+                    <flux:input wire:model.live='min' max="{{ $max }}" type="number" step="500"
+                        min="0" :placeholder="'Min'"></flux:input>
+                    <div class="">-</div>
+                    <flux:input wire:model.live='max' min="{{ $min }}" type="number" step="500"
+                        min="0" :placeholder="'Max'"></flux:input>
                 </div>
-            @endif
-            <div class="w-full transition-all">
-                <div class="grid grid-cols-1 gap-2 mt-4 sm:grid-cols-2 md:grid-cols-3">
+
+                <div class="mt-8">Freshness</div>
+                <flux:radio.group wire:model.live='freshness' class="">
+                    <flux:radio :label="'Fresh'" :value="'fresh '"></flux:radio>
+                    <flux:radio :label="'Frozen'" :value="'frozen'"></flux:radio>
+                </flux:radio.group>
+
+                <div class="flex gap-4 mt-4 justify-start">
+                    <button wire:click='resetFilter'
+                        class="cursor-pointer w-full py-1 rounded-md hover:bg-[#CFAF8D] text-sm bg-[#D4A373]">Reset</button>
+                </div>
+            </div>
+            {{-- @endif --}}
+            <div class="w-full transition-all bg-white p-2 rounded-xl shadow-md h-fit">
+                <div class="grid space-y-8 grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
                     @forelse ($products as $item)
                         <a href="{{ route('shop.show', ['slug' => $item->slug]) }}" class="text-center">
-                            <img src="{{ $item->image_url }}" alt="Chocolate Croissant"
-                                class="rounded-lg shadow mb-2">
-                            <p class="font-semibold">{{ $item->name }}</p>
-                            <p class="text-gray-500">Rp. {{ number_format($item->price, 0, ',', '.') }}</p>
+                            <img src="{{ $item->image_url }}" alt="{{ $item->name }}" class="rounded-lg shadow mb-2">
+
+                            <div class="px-2">
+                                <div class="flex justify-between items-center mb-2">
+                                    <p class="font-semibold text-xl">{{ $item->name }}</p>
+                                    <p class="text-gray-500">Category : {{ $item->freshness }}</p>
+                                </div>
+                                <p class="text-[#D4A373] text-left">Rp. {{ number_format($item->price, 0, ',', '.') }}
+                                </p>
+                            </div>
                         </a>
                     @empty
-                        <div class="h-80 px-4 md:px-12 flex justify-center items-center text-lg md:text-xl text-gray-600 col-span-3">
+                        <div
+                            class="h-80 px-4 md:px-12 flex justify-center items-center text-lg md:text-xl text-gray-600 col-span-3">
                             No products match your current filter. Please adjust your selection to explore our full
                             range of Breads and Pastries.
                         </div>
