fcf854ab553a0e959c93cfff4c054203c059ccf4
wijayy <mertawijaya77@gmail.com>
2025-12-11 17:47:41 +0800

    upload hosting 11dec25

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -45,7 +45,7 @@ class Cart extends Component
         $this->addresses = Auth::user()->addresses;
         $this->address = $this->addresses->first();
 
-        $this->outlets = Outlet::where('is_active', true)->get();
+        $this->outlets = Outlet::get();
         $this->outlet = $this->outlets->first();
 
         $this->setMinShippingDate();
